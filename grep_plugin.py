8278f06e4cdb465015977ca34e8a701fe9762f0d
Nik <nik@khilnani.org>
2014-08-19 03:23:39 -0400

    Create grep_plugin.py

---
+++ grep_plugin.py
@@ -0,0 +1,37 @@
+'''append:
+Append a string to a file
+
+Usage:
+  append [String] [File]
+  a [String] [File]
+'''
+
+from ... tools.toolbox import bash
+import os
+
+alias=['a']
+
+def main(self, line):
+    """Append to file"""
+    args = bash(line)
+    if args is None:
+      return
+    elif (len(args) != 2):
+      print "append usage: append [String] [File] or: a [String] [File]"
+    else:
+      content = args[0]
+      target = args[1]
+      if (os.path.isdir(target)):
+        print "append: %s: Is a directory" % target
+      elif (os.path.exists(target)):
+        try:
+          with open(target, 'a') as f:
+            f.write("\n")
+            f.write(content)
+            f.close()
+          print "Appending to: %s " % target
+          print ">>>>"
+          print content
+          print ">>>>"
+        except Exception:
+          print "append: Unable to write to file %s" % target
